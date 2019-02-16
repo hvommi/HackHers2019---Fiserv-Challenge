781136723ed2510f4d9b5acb9415a3075844a19b
VaishaliDevarakonda <47434297+VaishaliDevarakonda@users.noreply.github.com>
2019-02-16 16:55:54 -0500

    Update Budget.java

--- Budget.java
+++ Budget.java
@@ -1,8 +1,11 @@
-//SKELETON CODE: really basic version of what I want to calculate
+//SKELETON CODE: what's going on behind the GUI
+//this class calculates whether the user is above/below budget
+//also looks at what categories the user spends on the most
 
 package hackhers;
 
 import java.util.Scanner;
+import java.util.ArrayList;
 
 
 public class Budget {
@@ -12,6 +15,28 @@ public class Budget {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Budget Balance = new Budget();
+		//Categories C = new Categories();
+		ArrayList<String> category = new ArrayList<String>();
+		
+		//adds the possible categories into the array list
+		category.add("Groceries");
+		category.add("Recreation");
+		category.add("Housing");
+		category.add("Transportation");
+		category.add("Clothing");
+		category.add("Travel");
+		category.add("Other");
+		
+		//gets the values from .add into array
+		category.get(0);
+		category.get(1);
+		category.get(2);
+		category.get(3);
+		category.get(4);
+		category.get(5);
+		category.get(6);
+		
+		//prints out the array
 		Scanner input = new Scanner(System.in); //for integer/double inputs
 		Scanner stringInput = new Scanner (System.in); //for string inputs
 		
@@ -20,23 +45,32 @@ public class Budget {
 		double budgetedAmt = input.nextDouble();
 		
 		System.out.println("How many transactions have passed through for this month?");
-		 int amount = input.nextInt();
+		int amount = input.nextInt();
 		  
-		 while(amount != 0){
+		double total = 0; 
+		for (int i = 0; i < amount; i++)
+		 { 
 			  System.out.println("Please enter transaction amount: ");
 			  double t = input.nextDouble();
 			  
-			  double total  = t += t;
+			  System.out.println("Here are the possible categories: ");
+			  System.out.println(category);
+			  
+			  System.out.println("What category did you mostly spend this on?");
+			  String c = stringInput.nextLine();
+			  
+			  total = total + t;
+			  
+			  System.out.println(total);
 			  
 			  amtLeft = budgetedAmt - total;
-			  amount--;
+			  
 		  }
-
 		 
 		//calculate the budget and tell if the user is over/within
 		System.out.println("You have $" + amtLeft + " left to spend");
 		
-		if(amtLeft >= 0) {
+		if(amtLeft > 0) {
 			System.out.println("You are within your budget. Congrats!");
 		}else{
 			System.out.println("Careful! You are over budget!");
@@ -48,9 +82,8 @@ public class Budget {
 
 }
 
-
 /* what the class is doing:
-* accept the input for the double and amount spent
-* calculate the amount spent
+* accept the input for the budget, number of transactions, and transaction data
 * ask what categories user spends most on
+* 
 */
