d4f0196ebcee58775066f578aa8dbadf6cecce46
hvommi <47434263+hvommi@users.noreply.github.com>
2019-02-16 12:36:11 -0500

    Add files via upload

---
+++ HackHER/src/App.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+	File name: App.java
+	Short description: Run the program
+	IST 242 Assignment: L03 (week 5)
+	@author HIMANI VOMMI
+	@version 2.4.19 
+*/
+/**
+ *
+ * @author hvomm
+ */
+import Model.Model;
+public class App {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        //instantiate a new Model 
+        Model Model = new Model();
+        // TODO code application logic here
+    }
+    
+}
+
+//OUTPUT: 
+/*
+run:
+[Marcus Allen, 6'2", 209, Upper Marlboro, MD, Dr. Henry A. Wise, 2, S]
+[Marcus Allen, 6'2", 209, Upper Marlboro, MD, Dr. Henry A. Wise, 2, S]
+0 = name - Marcus Allen
+1 = height - 6'2"
+2 = weight - 209
+3 = hometown - Upper Marlboro, MD
+4 = highSchool - Dr. Henry A. Wise
+5 = number - 2
+6 = position - S
+[Marcus Allen, 6'2", 209, Upper Marlboro, MD, Dr. Henry A. Wise, 2, S]
+[Marcus Allen, 6'2", 209, Upper Marlboro, MD, Dr. Henry A. Wise, 2, S]
+BUILD SUCCESSFUL (total time: 0 seconds)
+*/
---
+++ HackHER/src/Model/Budget.java
@@ -0,0 +1,53 @@
+
+/**
+	File name: $({name}.java
+	Short description:
+	IST 242 Assignment:In class
+	@author HIMANI VOMMI
+	@version 1.16.19 
+*/
+
+package Model;
+
+
+
+
+
+
+/**
+ *
+ * @author hvomm
+ */
+public class Budget {
+
+
+	// Instance Variables -- define your private data
+
+
+	// Constructors
+	public Budget()
+	{
+		// initialize default values
+	}
+	public Budget(int data) // pass in data to initialize variables
+	{
+	}
+
+	// Set methods - one set method for each instance variable defined above
+	//             - purpose is to pass in a value stored in the private variable
+
+	// Get methods - one get method for each instance variable defined above
+	//             - purpose is to return the value stored in the private variable
+
+	// Additional methods -- such as for calculation, display
+
+	public String toString()
+	{
+		// return data as a String
+		return "";
+	}
+
+
+
+
+}
