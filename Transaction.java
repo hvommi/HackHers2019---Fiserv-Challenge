fbe59a812a93e5a70e96124c09b400aad7051f67
heliababz <44245561+heliababz@users.noreply.github.com>
2019-02-16 16:56:46 -0500

    Add files via upload

---
+++ Transaction.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hackathon;
+
+/**
+ *
+ * @author heliaaghababazadeh
+ */
+
+import java.util.Date;
+public class Transaction{
+    
+    private String category;
+    private double amount;
+    private Date dateCreated;
+    
+    Transaction(){
+    }
+    
+    Transaction(String category, double amount){
+        this.category = category;
+        this.amount = amount;
+        dateCreated = new Date();
+    }
+    
+    String getCategory(){
+        return category;
+    }
+    
+    double getAmount(){
+        return amount;
+    }
+    
+    Date getDate(){
+        return dateCreated;
+    }
+      
+}
