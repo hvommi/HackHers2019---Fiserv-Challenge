9c99516295a67c0f3f4e6bceca19847ed3f4e497
hvommi <47434263+hvommi@users.noreply.github.com>
2019-02-16 12:11:25 -0500

    Add files via upload

---
+++ App.java
@@ -0,0 +1,15 @@
+
+import Model.*;
+
+public class App
+{
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args)
+    {
+        Model model = new Model();
+    }
+
+}
